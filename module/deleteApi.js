ba392aed0024441d8516cc6f3220146fee74e2a2
pawanjoshi2312 <pawan.joshi.2312@gmail.com>
2020-08-25 19:46:52 +0530

    validation changes

--- module/deleteApi.js
+++ module/deleteApi.js
@@ -4,6 +4,9 @@ const empModel = require('../model/empModel');
 function deleteApi(req, res) {
 
     let id = req.body.id;
+    if(id==undefined){
+        return res.status(422).send('unprocessable entry');
+    }
     empModel.findByIdAndDelete(id, function (error) {
         if (error) {
             return res.status(404).send("not found");
