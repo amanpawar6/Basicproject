1598e6017a7639616b66c98a9791feff921436dc
Akshit Khajuria <khajurination@gmail.com>
2020-08-25 15:51:15 +0530

    Added square number api

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+function squaredResponse(req, res, next){
+  let ip = req.params.number?req.params.number:req.body.number;
+  let number = parseInt(ip);
+  if(isNaN(number)){
+    res.status(200).send("Not a number!");
+  }
+  else{
+    res.status(200).send({squaredNumber:number*number});
+  }
+}
+
+router.get('/square/:number', squaredResponse);
+router.get('/square/', squaredResponse);
+
 module.exports = router;
