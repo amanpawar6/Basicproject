fe0c40119f18deb58e23434e824f0c620012f96e
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2020-08-25 16:16:03 +0530

    added update api

---
+++ module/updateuser.js
@@ -0,0 +1,30 @@
+const emp = require("../models/empModel");
+
+
+async function updateprofile(req, res, next) {
+    try{
+      const Employee=await emp.findById(req.params.id);
+      if(!Employee){
+        return res.json({
+          success:false,
+          message:"Employee id doesnot exist",
+        }); 
+      }else{
+        let updateEmp=await emp.findByIdAndUpdate(req.params.id,req.body,{
+          new:true,
+          runValdator:true
+        });
+        res.json({
+          success:true,
+          message:"emp updated successfully",
+          student:updateEmp
+        });
+      }
+      }catch(err){
+        return(error);
+      }
+    }
+  
+
+
+    module.exports = {updateprofile}
