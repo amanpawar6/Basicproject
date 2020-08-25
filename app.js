3622d4151e8ba18b63c58a616aeb5cdd7e1cf5a1
amanpawar6 <aman.pawar.7.1997@gmail.com>
2020-08-25 14:11:24 +0530

    setting up project

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose=require('mongoose');
+var helmet=require('helmet');
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(helmet());
+
+//mongo db server connection
+
+mongoose.connect('mongodb://localhost/basicProject',{ useNewUrlParser: true ,useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connection established");
+});
+
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
